e3293308e92ec92e0469bf3d5a7771d6ad21ff36
Trujillo <trujillosz.jorge@gmail.com>
2026-01-14 17:44:05 -0600

    Update files in order to advance with more features on the Portfolio

--- Projects/Dice-Puzzle/index.js
+++ Projects/Dice-Puzzle/index.js
@@ -1,27 +1,34 @@
-var randomNumber1 = Math.random();
-randomNumber1 = randomNumber1 * 6;
-randomNumber1 = Math.floor(randomNumber1);
+function rollDice() {
 
-var randomNumber2 = Math.random();
-randomNumber2 = randomNumber2 * 6;
-randomNumber2 = Math.floor(randomNumber2);
+  var randomNumber1 = Math.random();
+  randomNumber1 = randomNumber1 * 6;
+  randomNumber1 = Math.floor(randomNumber1);
 
-var images = ["./images/dice1.png",
-  "./images/dice2.png",
-  "./images/dice3.png",
-  "./images/dice4.png",
-  "./images/dice5.png",
-  "./images/dice6.png"
-]
+  var randomNumber2 = Math.random();
+  randomNumber2 = randomNumber2 * 6;
+  randomNumber2 = Math.floor(randomNumber2);
 
-document.querySelector(".img1").src = images[randomNumber1];
+  var images = ["./images/dice1.png",
+    "./images/dice2.png",
+    "./images/dice3.png",
+    "./images/dice4.png",
+    "./images/dice5.png",
+    "./images/dice6.png"
+  ]
 
-document.querySelector(".img2").src = images[randomNumber2];
+  document.querySelector(".img1").src = images[randomNumber1];
 
-if (randomNumber1 > randomNumber2){
-  document.querySelector("h1").innerHTML = "🚩Player 1 Wins";
-} else if (randomNumber1 === randomNumber2){
-  document.querySelector("h1").innerHTML = "Draw!";
-} else{
-  document.querySelector("h1").innerHTML = "Player 2 Wins🚩";
+  document.querySelector(".img2").src = images[randomNumber2];
+
+  if (randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = "🚩Player 1 Wins";
+  } else if (randomNumber1 === randomNumber2){
+    document.querySelector("h1").innerHTML = "Draw!";
+  } else{
+    document.querySelector("h1").innerHTML = "Player 2 Wins🚩";
+  }
 }
+
+rollDice();
+
+document.querySelector("button").addEventListener("click", rollDice);
